052bf79eac2eb35385dd5cf3a4f4e5e426e0ebf6
Alex Ho <alexho1@asu.edu>
2021-02-23 21:54:55 -0500

    define views

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, 'myapp/index.html')
+
+
+def login(request):
+    return render(request, 'myapp/login.html')
+
+
+def register(request):
+    return render(request, 'myapp/registration.html')
